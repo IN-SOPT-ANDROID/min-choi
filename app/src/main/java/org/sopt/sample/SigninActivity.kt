eac43226337bb7a8a3a32d2af4226e16b27b3385
MIN <alsdud11737@naver.com>
2022-10-04 02:52:13 +0900

    필수과제완료

---
+++ app/src/main/java/org/sopt/sample/SigninActivity.kt
@@ -0,0 +1,45 @@
+package org.sopt.sample
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import org.sopt.sample.databinding.ActivitySigninBinding
+
+class SigninActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySigninBinding
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySigninBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnLogin.setOnClickListener {
+            if (binding.edtIdEnter.text.length > 6) {
+                Snackbar.make(binding.root, "로그인을 성공했습니다.", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+        registerSignup()
+        clickbtnSignup()
+    }
+
+    private fun registerSignup() {
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == RESULT_OK) {
+                    val userId = it.data?.getStringExtra("userId") ?: ""
+                    val userPassword = it.data?.getStringExtra("userPassword") ?: ""
+                    binding.edtIdEnter.setText(userId)
+                    binding.edtPasswordEnter.setText(userPassword)
+                }
+            }
+    }
+
+    private fun clickbtnSignup() {
+        binding.btnSignup.setOnClickListener { val intent = Intent(this, SignupActivity::class.java)
+            resultLauncher.launch(intent) }
+    }
+}
+
+
---
+++ app/src/main/java/org/sopt/sample/SignupActivity.kt
@@ -0,0 +1,30 @@
+package org.sopt.sample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sopt.sample.databinding.ActivitySignupBinding
+
+class SignupActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignupBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignupfinish.setOnClickListener {
+            val intent = Intent(this, SigninActivity::class.java)
+
+            if (binding.edtIdEnter.text.length > 6 || binding.edtIdEnter.text.length < 10 && binding.edtPasswordEnter.text.length > 8 || binding.edtPasswordEnter.text.length < 12) {
+                intent.putExtra("userId", binding.edtIdEnter.text.toString())
+                intent.putExtra("userPassword", binding.edtPasswordEnter.text.toString())
+                setResult(RESULT_OK, intent)
+                if (!isFinishing) {
+                    finish()
+                }
+            }
+        }
+    }
+}
+//if (edtInputId.length() > 6 || edtInputId.length() < 10 && edtInputPassword.length() > 8 || edtInputPassword.length() < 12) {
+//if (edtInputId.length() in 6..10 && edtInputPassword.length() in 9..11) {
